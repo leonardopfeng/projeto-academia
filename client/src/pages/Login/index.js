47e7b8e14de43cd2f70f157f175aac75a2983fab
Leonardo Pfeng <col.leonardopfeng@uniuv.edu.br>
2025-02-26 21:26:48 -0300

    feat: created a react project
    started the login page structure

---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './styles.css';
+
+export default function Login(){
+    return (
+        <div className="login-container">
+            <section className="form">
+                <form>
+                    <h1> Access your account</h1>
+                    <input placeholder="Username"></input>
+                    <input type="password" placeholder="Password"></input>
+
+                    <button type="submit" className="button">Login</button>
+                </form>
+            </section>
+        </div>
+
+    )
+}
